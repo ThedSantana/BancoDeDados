b9d3d99f91e60cef3ceecb0fa9d5b6f99cb90600
Lucas Mendes <lucas.mendes.go@gmail.com>
2016-08-31 21:31:47 -0400

    Initial commit

---
+++ app/src/main/java/com/github/lucas_mendes/bancodedados/InsereDado.java
@@ -0,0 +1,45 @@
+package com.github.lucas_mendes.bancodedados;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by lucas on 30/08/16.
+ */
+public class InsereDado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insere);
+
+        Button botao = (Button)findViewById(R.id.button);
+
+        final Intent intent = new Intent(InsereDado.this, Consulta.class);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BancoController crud = new BancoController(getBaseContext());
+                EditText titulo = (EditText)findViewById(R.id.editText);
+                EditText autor = (EditText)findViewById((R.id.editText2));
+                EditText editora = (EditText)findViewById(R.id.editText3);
+                String tituloString = titulo.getText().toString();
+                String autorString = autor.getText().toString();
+                String editoraString = editora.getText().toString();
+                String resultado;
+
+                resultado = crud.insereDado(tituloString,autorString,editoraString);
+
+                Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
+    }
+}
